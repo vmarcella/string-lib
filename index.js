09b034250e49c6adba5c3a080cad94ea0045befd
C3NZ <cenz@cenz.io>
2019-07-10 12:55:46 -0700

    [feat] add all capitalize functions

--- index.js
+++ index.js
@@ -1,14 +1,48 @@
-String.prototype.uppercaseFirst = function() {
-  return this[0].toUpperCase() + this.slice(1, this.length);
+String.prototype.capitalize = function() {
+  /*
+   * Capitalize the first letter of the current string.
+   */
+  const firstChar = this[0].toUpperCase();
+  return firstChar + this.slice(1, this.length);
 };
 
-String.prototype.uppercaseAll = function() {
-  outputList = [];
-  for (str of this.split(" ")) {
-    outputList.push(str.uppercaseFirst());
+String.prototype.allCaps = function() {
+  /*
+   * Uppercase every character within the string.
+   */
+  return this.toUpperCase();
+};
+
+String.prototype.captializeWords = function() {
+  /*
+   * Uppercase every word within the string
+   * (Not including: the, in, a, an, and, but, for, at, by, from)
+   */
+  exclusion_words = [
+    "the",
+    "in",
+    "a",
+    "an",
+    "and",
+    "but",
+    "for",
+    "at",
+    "by",
+    "from"
+  ];
+
+  capitalized_word = [];
+
+  // Iterate through our words, check if they're an exclusion word,
+  // and then capitalzie them if they're not.
+  for (word of this.split(" ")) {
+    if (!exclusion_words.includes(word)) {
+      word = word.capitalize();
+    }
+    captializeWords.push(word.toUpperCase());
   }
 
-  return outputList.join(" ");
+  return captializedWords.join("");
 };
 
 String.prototype.uppercaseEveryOther = function() {
