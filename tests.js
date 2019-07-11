fd0961312b417f618cc2d04c6aca49213918f66b
C3NZ <cenz@cenz.io>
2019-07-10 19:29:00 -0700

    [feat] add tests for allCaps()

--- tests.js
+++ tests.js
@@ -4,12 +4,26 @@ require("mocha");
 const assert = require("assert");
 require("./");
 
-describe("Extending the string functionality", () => {
+describe("Extending the String type functionality", () => {
   it("should captialize the first letter", () => {
     assert.equal("Abc", "abc".capitalize());
     assert.equal("Details", "details".capitalize());
     assert.equal(" ", " ".capitalize());
     assert.equal("HI", "hI".capitalize());
     assert.equal("YeSaNdNo", "yeSaNdNo".capitalize());
+
+    assert.notEqual("aBC", "aBC".capitalize());
+  });
+
+  it("should make all characters caps", () => {
+    assert.equal("ABC", "abc".allCaps());
+    assert.equal("FOO FOO BAR", "foo foo bar".allCaps());
+    assert.equal(
+      "EXTREMELY LONG STRING THAT IS EXTREMELY LONG",
+      "extremely long string that is extremely long".allCaps()
+    );
+
+    assert.notEqual("YES AnD NO", "yes and no".allCaps());
+    assert.notEqual("TaLkInG", "talking".allCaps());
   });
 });
