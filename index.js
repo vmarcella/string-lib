c2d68223e39b4a5bd717e5d06fe17eef902455b3
C3NZ <cenz@cenz.io>
2019-07-10 17:59:50 -0700

    [feat] finish the rest of the string functions and addd documentation to explain what each does

--- index.js
+++ index.js
@@ -45,6 +45,101 @@ String.prototype.captializeWords = function() {
   return captializedWords.join("");
 };
 
+String.prototype.oddCaps = function() {
+  /*
+   * Uppercase every other odd character not including white space.
+   */
+  let counter = 0;
+  output_word = [];
+
+  // Iterate through every character capitalize every odd one.
+  for (char of this.split()) {
+    if (char !== " ") {
+      char = counter % 2 == 0 ? char : char.toUpperCase();
+      counter += 1;
+    }
+    output_word.push(char);
+  }
+
+  return output_word.join("");
+};
+
+String.prototype.evenCaps = function() {
+  /*
+   * Uppercase every even character not including white space.
+   */
+  let counter = 0;
+  output_word = [];
+
+  for (char of this.split()) {
+    if (char !== " ") {
+      char = counter % 2 == 0 ? char.toUpperCase() : char;
+      counter += 1;
+    }
+
+    output_word.push(char);
+  }
+
+  return output_word.join("");
+};
+
+String.prototype.removeExtraWhiteSpace = function() {
+  /*
+   * Remove any extra whitespace throughout the string.
+   */
+
+  // Regex pattern that searches for any global occurence of
+  // continuations of two or more spaces.
+  const pattern = /[ ]{2,}/g;
+  let newStr = this.replace(pattern, " ");
+  return newStr.trim();
+};
+
+String.prototype.kabobCase = function() {
+  /*
+   * Kabob case the current string if it has spaces.
+   */
+  const trimmedStr = this.removeExtraWhiteSpace();
+  const pattern = /\s/g;
+  const newStr = this.replace(trimmedStr, "-");
+  return newStr;
+};
+
+String.prototype.snakeCase = function() {
+  /*
+   * Snake case the current string if it has spaces.
+   */
+  const trimmedStr = this.removeExtraWhiteSpace();
+  const pattern = /\s/g;
+  const newStr = this.replace(trimmedStr, "_");
+  return newStr;
+};
+
+String.prototype.camelCase = function() {
+  /*
+   * Convert the current string to camel case if it has spaces.
+   */
+  const trimmedStr = this.removeExtraWhiteSpace();
+  const outputWord = [];
+
+  // Iterate through all of the characters of the trimmed
+  // string.
+  for (let i = 0; i < trimmedStr.length; i += 1) {
+    let currChar = trimmedStr[i];
+
+    // If we're not at the first index and the previous character
+    // equals a space, uppercase the current character.
+    if (i != 0 && trimmedStr[i - 1] === " ") {
+      currChar = currChar.toUpperCase();
+    }
+
+    // If the character isn't a space, push it to the new word list
+    if (currChar !== " ") {
+      outputWord.push(currChar);
+    }
+  }
+};
+
 String.prototype.uppercaseEveryOther = function() {
   outputList = [];
   for (let i = 0; i < this.length; i += 1) {
@@ -63,12 +158,6 @@ String.prototype.removeWhiteSpace = function() {
   }
 
   return outputList.join("");
-};
-
-String.prototype.removeExtraWhiteSpace = function() {
-  const pattern = /[ ]{2,}/g;
-  newStr = this.replace(pattern, " ");
-  return newStr.trim();
 };
 
 console.log("yeet".uppercaseFirst());
