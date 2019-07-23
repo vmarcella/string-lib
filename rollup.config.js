2a4eb55b796be7d5659bbf0f5203c8c68786d279
C3NZ <cenz@cenz.io>
2019-07-23 14:13:27 -0700

    [add] terser to minify code and then fix some code to work for the tests

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,21 @@
-export default {
-  input: "index.js",
-  output: {
-    file: "umd/index.js",
-    format: "umd",
-    name: "stringModule"
+import { terser } from "rollup-plugin-terser";
+
+export default [
+  {
+    input: "index.js",
+    plugins: [terser()],
+    output: {
+      file: "umd/index.js",
+      format: "umd",
+      name: "stringModule",
+      esModule: false
+    }
+  },
+  {
+    input: "index.js",
+    output: {
+      file: "esm/index.js",
+      format: "esm"
+    }
   }
-};
+];
